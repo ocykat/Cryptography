35821cee431cc0aed8f53448cdc251457c66d398
nhatnguyen <nhatnguyen.sg.vn@gmail.com>
2018-01-17 23:18:47 +0700

    add comments

--- cipher/caesar.py
+++ cipher/caesar.py
@@ -4,13 +4,21 @@ from util import codec
 
 def encrypt(s, n):
     res = ""
+
     for i in range(len(s)):
         if text.is_letter(s[i]):
+            # convert letter to code
             x = codec.to_number(s[i])
+
+            # process
             x = (x + n) % 26
+
+            # convert code to letter
             res += codec.to_letter(x)
+
         else:
             res += s[i]
+
     return res
 
 
@@ -18,9 +26,16 @@ def decrypt(s, n):
     res = ""
     for i in range(len(s)):
         if text.is_letter(s[i]):
+            # convert letter to code
             x = codec.to_number(s[i])
+
+            # process
             x = (x - n) % 26
+
+            # convert code to letter
             res += codec.to_letter(x)
+
         else:
             res += s[i]
+
     return res
