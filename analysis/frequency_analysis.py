89cdf1a3d4f10ad26bc60d9b271373b18e2b8104
nhatnguyen <nhatnguyen.sg.vn@gmail.com>
2018-01-18 11:29:59 +0700

    add plotting features

--- analysis/frequency_analysis.py
+++ analysis/frequency_analysis.py
@@ -1,4 +1,4 @@
-import util
+from util import util
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -15,13 +15,38 @@ class FrequencyAnalysis:
                 charcode = util.to_number(char)
                 self.frequency[charcode] += 1
 
-    def plot(self):
+    def plot(self, title):
         letters = [util.to_letter(i) for i in range(26)]
         xpos = np.arange(len(letters))
+
         width = 0.5
-        plt.bar(xpos, self.frequency, width)
+        bars = plt.bar(xpos, self.frequency, width)
+
         plt.xticks(xpos, letters)
+
         plt.xlabel('Letter')
         plt.ylabel('Frequency')
-        plt.title('')
+
+        plt.title(title)
+
+        def label():
+            """
+            Attach a text label above each bar displaying its height
+            """
+            for bar in bars:
+                x = bar.get_x()
+                height = bar.get_height()
+                plt.text(x + width / 2., height + 0.1, '%d' % int(height),
+                         ha='center', va='bottom')
+
+        label()
+
+        def max_frequency():
+            max_freq = self.frequency[0]
+            for i in range(1, len(self.frequency)):
+                if max_freq < self.frequency[i]:
+                    max_freq = self.frequency[i]
+            return max_freq
+
+        plt.ylim([0, max_frequency() * 1.1])
         plt.show()
